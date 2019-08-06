944897bef54fd1bc6e4b7dd692fb0248ddfac849
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-06 11:56:11 -0400

    Add project files.

---
+++ Entity_FrameworkProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Entity_FrameworkProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
